edf4d62f53795636cabb781b45e9b3ff5349e69b
Wanderley Carvalho <wanderley.filho@localiza.com>
2021-02-06 13:34:06 -0300

    Estruturar de pastas e adicionar navigator

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigator from './MainNavigator';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
